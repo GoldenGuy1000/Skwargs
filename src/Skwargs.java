f1a44179fdbbcf19177bc731269c4c824d700de6
GoldenGuy1000 <GoldenGuy1000@gmail.com>
2020-10-31 16:18:46 -0700

    This is the first commit of Skwargs. In it you input defaults and the kwargs then get them from it using the get method.
    when initializing you need to input 2 matched String[]
    1 of them is the variables and the other is the default values.
    you also need to input another String[] that has the formating
    "variable=value"
    
    this will update the hashmap of inputted defaults with the new values inputted in the kwargs
    
    the .get has 7 permutations
    .get(String var) // returns the value as a string
    .getInt(String var) // returns the value as a Int
    .getLong(String var) // returns the value as a Long
    .getFloat(String var) // returns the value as a Float
    .getDouble(String var) // returns the value as a Double
    .getBool(String var) // returns the value as a Boolean (true if the text says true else false)
    
    .get works by looking up the item in the dict and parsing the string it gets from that to the data type you want
    
    there's also help which does nothing at the moment

---
+++ src/Skwargs.java
@@ -0,0 +1,77 @@
+import java.util.*;
+
+public class Skwargs {
+    Map<String, String> Variables = new HashMap<>();
+
+    public Skwargs(String[] variables, String[] defaultValues, String[] kwargs) {
+        // setting up defaults
+        for(int i = 0; i < variables.length; i++ ) {
+            this.Variables.put(variables[i].replace(" ", ""),defaultValues[i].replace(" ", ""));
+        }
+        // updating dictionary with kwargs
+        for(String kwarg : kwargs) {
+            String variable = kwarg.split("=")[0].trim();
+            String newValue = kwarg.split("=")[1].trim();
+            this.Variables.put(variable,newValue);
+        }
+    }
+
+
+// help
+    public void help() {
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+    }
+// specific help
+    public void help(String method) {
+
+    }
+
+    private String varToValue (String var) { return this.Variables.get(var); }
+// get string
+    public String get (String var) { return varToValue(var);}
+// get int
+    public int getInt (String var) { return Integer.parseInt(varToValue(var)); }
+// get long
+    public long getLong (String var) { return Long.parseLong(varToValue(var)); }
+// get float
+    public float getFloat (String var) {return Float.parseFloat(varToValue(var));}
+// get double
+    public double getDouble (String var) { return Double.parseDouble(varToValue(var)); }
+// get boolean
+    public boolean getBool (String var) {
+        if (varToValue(var).toLowerCase().equals("true")) {
+            return true;
+        }
+        else if (varToValue(var).toLowerCase().equals("false")) {
+            return false;
+        }
+        else {
+            return false;
+        }
+    }
+
+}
+
+// the following code is just an example use of Skwargs and at the top it's showing how to set it up
+
+//    private static double math (double num1, double num2, String ... kwargs) {
+//        String[] variables = new String[]{"typeOfMath","intMath"};
+//        String[] defaults = new String[]{"addition","false"};
+//        Skwargs vars = new Skwargs(variables, defaults, kwargs);
+//
+//        if (vars.get("typeOfMath").equals("addition")) {
+//            if (vars.getBool("intMath")) {
+//                return (int)num1 + (int)num2;
+//            }
+//            else {
+//                return num1 + num2;
+//            }
+//        }
+//
+//        else {
+//            return 0;
+//        }
+//    }
