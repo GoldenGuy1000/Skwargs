d7ce3efe1d69f4ceba893681f2864e5e7cf8980c
GoldenGuy1000 <GoldenGuy1000@gmail.com>
2020-11-01 01:07:49 -0700

    made the method public

--- src/Skwargs.java
+++ src/Skwargs.java
@@ -1,7 +1,7 @@
 import java.util.Map;
 
 public class Skwargs {
-    static Map<String, String> generate(Map<String, String> dict, String[] kwargs) {
+     public static Map<String, String> generate(Map<String, String> dict, String[] kwargs) {
         for(String kwarg : kwargs) {
             String key = kwarg.split("=")[0].trim();
             String newValue = kwarg.split("=")[1].trim();
