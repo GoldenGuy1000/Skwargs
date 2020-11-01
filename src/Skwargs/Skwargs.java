81829fd642828ec82834b087fae82ca5474f8cb0
GoldenGuy1000 <GoldenGuy1000@gmail.com>
2020-11-01 01:28:21 -0800

    Added String key - integer value map support
    Using black magic I've beat the system. just add:
    , int ... nothing
    to the end and all of my problems are solved (with the whole type erasure thing causing it to error)

--- src/Skwargs/Skwargs.java
+++ src/Skwargs/Skwargs.java
@@ -2,10 +2,18 @@ package Skwargs;
 import java.util.Map;
 
 public class Skwargs {
-     public static Map<String, String> generate(Map<String, String> dict, String[] kwargs) {
+     public static Map<String, String> generate(Map<String, String> dict, String[] kwargs, String ... nothing) {
         for(String kwarg : kwargs) {
             String key = kwarg.split("=")[0].trim();
             String newValue = kwarg.split("=")[1].trim();
+            dict.put(key, newValue);
+        }
+        return dict;
+    }
+    public static Map<String, Integer> generate(Map<String, Integer> dict, String[] kwargs, int ... nothing) {
+        for(String kwarg : kwargs) {
+            String key = kwarg.split("=")[0].trim();
+            int newValue = Integer.parseInt(kwarg.split("=")[1].trim());
             dict.put(key, newValue);
         }
         return dict;
